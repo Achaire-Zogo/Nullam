8dedfe8542e817fc25f61b4510d030f443d66378
dylan-zogo <dylanabouma@gmail.com>
2021-01-29 12:48:43 +0100

    second pusch

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,7 @@
-
 var compteurImage=1;
-var totalImage=6;
+var totalImage=10;
 
-function slider(x){
+function silder(x){
 
 var image=document.getElementById('img');
 compteurImage=compteurImage + x;
@@ -11,6 +10,7 @@ image.src="./static/images/nullam"+compteurImage+".jpeg";
 	if (compteurImage<1){compteurImage=totalImage;}
 	
 }
+
 function silderAuto(){
 
 var image=document.getElementById('img');
@@ -22,3 +22,16 @@ image.src="./static/images/nullam"+compteurImage+".jpeg";
 }
 
 window.setInterval(silderAuto,3000);
+
+function silderAuto1(){
+
+var image=document.getElementById('img1');
+compteurImage=compteurImage + 1;
+image.src="./static/images/nullam"+compteurImage+".jpeg";
+	if (compteurImage>=totalImage){compteurImage=1;}
+	if (compteurImage<1){compteurImage=totalImage;}
+		
+}
+	
+window.setInterval(silderAuto1,2000);
+	
