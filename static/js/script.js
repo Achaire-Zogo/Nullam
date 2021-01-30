ef8a4418ed1d549d6f14ce1bf206f730da8f2023
dylan-zogo <dylanabouma@gmail.com>
2021-01-30 08:52:20 +0100

    seven

--- static/js/script.js
+++ static/js/script.js
@@ -1,50 +1,66 @@
-var compteurImage=1;
-var totalImage=10;
+var compteurImage = 1;
+var totalImage = 10;
 
-function silder(x){
+function silder(x) {
 
-var image=document.getElementById('img');
-compteurImage=compteurImage + x;
-image.src="./static/images/nullam"+compteurImage+".jpeg";
-	if (compteurImage>=totalImage){compteurImage=1;}
-	if (compteurImage<1){compteurImage=totalImage;}
-	
+    var image = document.getElementById('img');
+    compteurImage = compteurImage + x;
+    image.src = "./static/images/nullam" + compteurImage + ".jpeg";
+    if (compteurImage >= totalImage) {
+        compteurImage = 1;
+    }
+    if (compteurImage < 1) {
+        compteurImage = totalImage;
+    }
+
 }
 
-function silderAuto(){
+function silderAuto() {
 
-var image=document.getElementById('img');
-compteurImage=compteurImage + 1;
-image.src="./static/images/nullam"+compteurImage+".jpeg";
-	if (compteurImage>=totalImage){compteurImage=1;}
-	if (compteurImage<1){compteurImage=totalImage;}
-	
+    var image = document.getElementById('img');
+    compteurImage = compteurImage + 1;
+    image.src = "./static/images/nullam" + compteurImage + ".jpeg";
+    if (compteurImage >= totalImage) {
+        compteurImage = 1;
+    }
+    if (compteurImage < 1) {
+        compteurImage = totalImage;
+    }
+
 }
 
-window.setInterval(silderAuto,12000);
+window.setInterval(silderAuto, 12000);
 
-function silderAuto1(){
+function silderAuto1() {
 
-	var image=document.getElementById('img1');
-	compteurImage=compteurImage + 1;
-	image.src="./static/images/nullam"+compteurImage+".jpeg";
-		if (compteurImage>=totalImage){compteurImage=1;}
-		if (compteurImage<1){compteurImage=totalImage;}
-		
-	}
-	
-	window.setInterval(silderAuto1,8000);
+    var image = document.getElementById('img1');
+    compteurImage = compteurImage + 1;
+    image.src = "./static/images/nullam" + compteurImage + ".jpeg";
+    if (compteurImage >= totalImage) {
+        compteurImage = 1;
+    }
+    if (compteurImage < 1) {
+        compteurImage = totalImage;
+    }
 
-function silderAuto2(){
+}
 
-	var image=document.getElementById('img2');
-	compteurImage=compteurImage + 1;
-	image.src="./static/images/nullam"+compteurImage+".jpeg";
-		if (compteurImage>=totalImage){compteurImage=1;}
-		if (compteurImage<1){compteurImage=totalImage;}
-			
-	}
-		
-window.setInterval(silderAuto2,4000);
+window.setInterval(silderAuto1, 8000);
+
+function silderAuto2() {
+
+    var image = document.getElementById('img2');
+    compteurImage = compteurImage + 1;
+    image.src = "./static/images/nullam" + compteurImage + ".jpeg";
+    if (compteurImage >= totalImage) {
+        compteurImage = 1;
+    }
+    if (compteurImage < 1) {
+        compteurImage = totalImage;
+    }
+
+}
+
+window.setInterval(silderAuto2, 4000);
 
 
