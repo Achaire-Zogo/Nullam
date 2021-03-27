0b93dc81f140f2f5745632c891f2665606ee89bd
dylan-zogo <dylanabouma@gmail.com>
2021-03-27 23:19:48 +0100

    27/03

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
+from django.contrib import messages
+
 
 def login(request):
     return render(request, 'accounts/login.html')
@@ -7,7 +12,18 @@ def logout(request):
     return redirect(request, 'accounts/logout.html')
 
 def register(request):
-    return render('register.html')
+    form = CreateUserForm()
+
+    if request.method== 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'account was created for ' + user)
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
--- conf/settings.py
+++ conf/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,10 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
+
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
